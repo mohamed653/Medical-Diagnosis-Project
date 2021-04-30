a3260010ae4943b7b94386442ed20f0377fa262d
omar sawy <46130283+omarsawy123@users.noreply.github.com>
2021-04-30 14:29:31 +0200

    Controller Example

--- Medical Diagnosis/Controllers/LoginController.cs
+++ Medical Diagnosis/Controllers/LoginController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Medical_Diagnosis.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Medical_Diagnosis.Controllers
 {
     public class LoginController : Controller
     {
+
+        private readonly MedicalDiagnosisContext _context;
+
+        public LoginController(MedicalDiagnosisContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Test()
+        {
+            Patient patient = _context.Patients.FirstOrDefault(p => p.Id == 1);
+
+            return Json(patient);
         }
     }
 }
